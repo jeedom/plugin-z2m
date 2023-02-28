09328b8fdf11d44bf7982d5429ec922aa1d52f45
Loïc <loic@jeedom.com>
2023-02-28 14:12:23 +0100

    bugfix

--- core/class/z2m.class.php
+++ core/class/z2m.class.php
@@ -609,27 +609,30 @@ class z2m extends eqLogic {
 
   /*     * *********************Methode d'instance************************* */
   public function createCmd($_infos, $_type = null) {
-    $cmd_ref = self::getCmdConf($_infos, null, $_type);
-    $logical = $_infos['name'];
-    if (isset($_infos['endpoint'])) {
-      $logical .= '_' . $_infos['endpoint'];
+    $link_cmd_id = null;
+    if ($_infos['type'] != 'composite') {
+      $cmd_ref = self::getCmdConf($_infos, null, $_type);
+      $logical = $_infos['name'];
+      if (isset($_infos['endpoint'])) {
+        $logical .= '_' . $_infos['endpoint'];
+      }
+      $cmd = $this->getCmd('info', $logical);
+      if (!is_object($cmd)) {
+        $cmd = new z2mCmd();
+        $cmd->setLogicalId($logical);
+        utils::a2o($cmd, $cmd_ref);
+      }
+      if (isset($_infos['endpoint'])) {
+        $cmd->setConfiguration('endpoint', $_infos['endpoint']);
+      }
+      $cmd->setEqLogic_id($this->getId());
+      try {
+        $cmd->save();
+      } catch (\Throwable $th) {
+        log::add('z2m', 'debug', 'Can not create cmd ' . json_encode(utils::o2a($cmd)) . ' => ' . $th->getMessage());
+      }
+      $link_cmd_id = $cmd->getId();
     }
-    $cmd = $this->getCmd('info', $logical);
-    if (!is_object($cmd)) {
-      $cmd = new z2mCmd();
-      $cmd->setLogicalId($logical);
-      utils::a2o($cmd, $cmd_ref);
-    }
-    if (isset($_infos['endpoint'])) {
-      $cmd->setConfiguration('endpoint', $_infos['endpoint']);
-    }
-    $cmd->setEqLogic_id($this->getId());
-    try {
-      $cmd->save();
-    } catch (\Throwable $th) {
-      log::add('z2m', 'debug', 'Can not create cmd ' . json_encode(utils::o2a($cmd)) . ' => ' . $th->getMessage());
-    }
-    $link_cmd_id = $cmd->getId();
 
     if ($_infos['access'] == 7 || $_infos['access'] == 3) {
       foreach (self::$_action_cmd as $k => $v) {
