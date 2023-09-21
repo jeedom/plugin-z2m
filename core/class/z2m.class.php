c61ce4bf86faed7cafa28ac6be8905053296f1e6
Loïc <1536036+zoic21@users.noreply.github.com>
2023-09-21 22:52:50 +0200

    Update z2m.class.php

--- core/class/z2m.class.php
+++ core/class/z2m.class.php
@@ -191,7 +191,7 @@ class z2m extends eqLogic {
     $configuration['advanced']['last_seen'] = 'ISO_8601';
 
     if(!file_exists($data_path . '/coordinator_backup.json') && !isset($configuration['advanced']['network_key'])){
-        $configuration['advanced']['network_key'] = 'GENERATE';
+       // $configuration['advanced']['network_key'] = 'GENERATE';
     }
 
     if (config::byKey('z2m_auth_token', 'z2m', '') == '') {
