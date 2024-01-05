27f5eda11d631cbef7562bddc63d47a30624c756
Loïc <1536036+zoic21@users.noreply.github.com>
2024-01-05 17:13:19 +0100

    Update configuration.php

--- plugin_info/configuration.php
+++ plugin_info/configuration.php
@@ -56,17 +56,26 @@ if (!isConnect()) {
           <option value="none">{{Aucun}}</option>
           <option value="auto">{{Auto}}</option>
           <option value="gateway">{{Passerelle distante}}</option>
-          <option value="/dev/ttyS2">{{Atlas}}</option>
+          <option value="/dev/ttyS2">{{Atlas (/dev/ttyS2)}}</option>
+          <option value="/dev/ttyLuna-Zigbee">{{Luna Zigbee (/dev/ttyLuna-Zigbee)}}</option>
           <?php
-          foreach (ls('/dev/', 'tty*') as $value) {
+	  	  $findPort = [];
+          /*foreach (ls('/dev/', 'tty*') as $value) {
+            if($value == 'tty' || preg_match('/tty\d/', $value) || preg_match('/ttyS\d/', $value) || preg_match('/ttyFIQ\d/', $value)){
+             	continue; 
+            }
+	        $findPort['/dev/' . $value] = '/dev/' . $value;  
             if ($value == "ttyLuna-Zigbee") {
               echo '<option value="/dev/' . $value . '">Luna Zigbee</option>';
             } else {
               echo '<option value="/dev/' . $value . '">/dev/' . $value . '</option>';
             }
-          }
+          }*/
           foreach (jeedom::getUsbMapping() as $name => $value) {
-            echo '<option value="' . $name . '">' . $name . ' (' . $value . ')</option>';
+            if(isset($findPort[$value])){
+                continue;
+            }
+            echo '<option value="' . $value . '">' . $name . ' (' . $value . ')</option>';
           }
           ?>
         </select>
