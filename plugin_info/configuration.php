b57e8b4a99c42e09edf87ee9b81d468066d43b30
Loïc <1536036+zoic21@users.noreply.github.com>
2024-05-02 09:05:04 +0200

    Update configuration.php

--- plugin_info/configuration.php
+++ plugin_info/configuration.php
@@ -91,6 +91,7 @@ if (!isConnect()) {
         <select class="configKey form-control" data-l1key="controller" id="sel_z2mControllerType">
           <option value="ti">{{ZNP/TI}}</option>
           <option value="ezsp">{{EZSP (Atlas/Luna/Smart)}}</option>
+	  <option value="ember">{{Ember}}</option>	
           <option value="deconz">{{Deconz/Conbee}}</option>
           <option value="conbee_3">{{Conbee 3}}</option>
           <option value="raspbee_2">{{Raspbee 2}}</option>
