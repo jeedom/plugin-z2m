d5e2acb64d7623c1dc1e888a8023176c0cb28eeb
Loïc <1536036+zoic21@users.noreply.github.com>
2024-09-09 16:37:28 +0200

    fix message

--- desktop/modal/firmware_update.php
+++ desktop/modal/firmware_update.php
@@ -20,8 +20,7 @@ if (!isConnect('admin')) {
 ?>
 <div id='div_alertFirmwareUpdate' style="display: none;"></div>
 <legend>{{Mise à jour}}</legend>
-<div class="alert alert-info">{{IMPORTANT : seul les clefs Elelabs peuvent être mises à jour par Jeedom actuellement}}</div>
-<div class="alert alert-info">{{IMPORTANT : NE PAS UTILISER SUR LA LUNA au risque de briquer votre contrôleur Zigbee}}</div>
+<div class="alert alert-info">{{IMPORTANT : seul les clefs Elelabs et les box Luna peuvent être mises à jour par Jeedom actuellement}}</div>
 <form class="form-horizontal">
   <fieldset>
     <div class="form-group">
