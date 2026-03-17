3fdfcb293c1035096ccfad77dcf2977d47b30293
Loïc <1536036+zoic21@users.noreply.github.com>
2026-03-17 11:15:36 +0100

    Update install.php

--- plugin_info/install.php
+++ plugin_info/install.php
@@ -31,11 +31,7 @@ function z2m_install() {
 }
 
 function z2m_update() {
-  if(config::byKey('port', 'z2m') == '/dev/ttyLuna-Zigbee'){
-    config::save('wanted_z2m_version', '1.42.0', 'z2m');
-  }else{
-    config::save('wanted_z2m_version', '', 'z2m');
-  }
+  config::save('wanted_z2m_version', '', 'z2m');
   $plugin = plugin::byId('z2m');
   if (config::byKey('z2m::mode', 'z2m', 'local') == 'local') {
     $plugin->dependancy_changeAutoMode(1);
